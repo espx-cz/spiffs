fcb7232c2cbac64f9f94fd254eefb8656a5ec3e2
Martin <maly@maly.cz>
2024-05-12 09:04:55 +0200

    V01

--- spiffs.js
+++ spiffs.js
@@ -1,24 +1,24 @@
-import { execSync, spawn } from 'child_process';
+#! /usr/bin/env node
+
+import { spawn } from 'child_process';
 import { promises as fs } from 'fs';
 import { program } from 'commander';
 import path from 'path';
 import {
     fileURLToPath
 } from 'url';
-import { get } from "http";
-import { type } from "os";
 const __filename = fileURLToPath(
     import.meta.url);
 const __dirname = path.dirname(__filename);
-//console.log("DIRNAME",__dirname);
 
 
 const doCmd = async (cmd) => new Promise((resolve, reject) => {
+
     //split cmd to cmd and params
     let cmdArr = cmd.split(' ');
     let cmdName = cmdArr[0];
     let params = cmdArr.slice(1);
-    //console.log(cmdName, params)
+
     let n = spawn(path.resolve(__dirname,cmdName), params);
     n.stdout.on('data', (data) => {
         console.log(data.toString());
@@ -36,18 +36,9 @@ const doCmd = async (cmd) => new Promise((resolve, reject) => {
         console.error(err);
         reject(err);
     });
-    /*
-    n.on('exit', (code) => {
-        console.log(`child process exited with code ${code}`);
-        resolve(code);
-    });
-    */
 
-}
+    }
 );
-
-let port="COM18"
-//let speed=921600
 
 const checkPort = () => {
     if (prog.opts().port === undefined) {
@@ -110,42 +101,8 @@ const listSpiffs = async (spiffsFile) => {
     await doCmd(cmd);
 }
 
-const main = async () => {
-    // call esptool.exe to read ESP partition table: esptool.exe --port COM18 read_flash 0x8000 0x1000 ./partition_table.bin
-    await readPartitionTable();
-
-    let data = await fs.readFile('./partition_table.bin');
-
-    //parse partition_table.bin
-    //treat it as binary file
-    //search for 0xaa 0x50 0x01 0x82 sequence
-    //if found, read the next 4 bytes as start address of spiffs and next 4 bytes as size of spiffs
-    let startAddress = 0;
-    let size = 0;
-    for(let i=0;i<data.length-4;i++){
-        if(data[i]===0xaa && data[i+1]===0x50 && data[i+2]===0x01 && data[i+3]===0x82){
-            startAddress = data.readUInt32LE(i+4);
-            size = data.readUInt32LE(i+8);
-            break;
-        }
-    }
-    console.log(`startAddress: 0x${startAddress.toString(16)}, size: 0x${size.toString(16)}`);
-    //download spiffs.bin
-    //await readSpiffs("./spiffs.bin", startAddress, size);
-
-    await unpackSpiffs("./spiffs.bin", "./dataFromEsp");
-
-    //make spiffs image from "./data" by mkspiffs.exe - mkspiffs -c data data.spiffs -p 256 -b 4096 -s ${size}
-    await makeSpiffs("./data", "./data.spiffs", size);
-
-    //flash it back to ESP32 by esptool.exe --port COM18 write_flash 0x${startAddress.toString(16)} ./data.spiffs
-    //await writeSpiffs("./data.spiffs", startAddress, size);
-}
-
-//main().then(()=>{process.exit(0)}).catch((e)=>{console.error(e);process.exit(1)});
 
 const prog = program
-//console.log(program)
 
 prog
  .version('0.0.1')
